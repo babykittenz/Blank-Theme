5b35d300e98c80487962d35536f9c407bd8987e0
Michael Kidby <kidbymichaelb@gmail.com>
2021-08-25 18:10:06 -0400

    debug

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php get_header(); ?>
 
-<?php if(have+posts()) {?>
+<?php if(have_posts()) {?>
     <?php  while(have_posts()) {?>
 
         <?php the_post(); ?>
+        <h2>
+            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"> <?php the_title() ?> </a>
+        </h2>
     <?php }?>
 
 <?php } else { ?>
