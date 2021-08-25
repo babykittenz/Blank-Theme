6d35477a33cddf1897727505739e31a7b9e8d594
Michael Kidby <kidbymichaelb@gmail.com>
2021-08-25 17:54:09 -0400

    inital commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' )?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://fonts.google.com/specimen/Raleway?selection.family=Raleway">
+        <title>Blank WordPress Theme | Mill Mountain Digital</title>
+        <?php wp_head(); ?>
+    </head>
+    <body>
