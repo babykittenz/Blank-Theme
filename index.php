3a66ac4fa9b2d6e18c8b224c9953eeb62700db29
Michael Kidby <kidbymichaelb@gmail.com>
2021-08-25 18:06:12 -0400

    the loop

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 
+<?php if(have+posts()) {?>
+    <?php  while(have_posts()) {?>
 
+        <?php the_post(); ?>
+    <?php }?>
+
+<?php } else { ?>
+    <p>No Posts</p>
+<?php } ?>
 
 <?php get_footer(); ?>
