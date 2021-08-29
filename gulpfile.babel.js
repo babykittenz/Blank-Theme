5e12843d2faed6420f35478abb832fedf548138f
Michael Kidby <kidbymichaelb@gmail.com>
2021-08-28 23:41:30 -0400

    last commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
+//import imagemin from 'gulp-imagemin'; //npm i gulp-imagemin@7.1.0
 
 
 
@@ -14,12 +15,18 @@ const paths = {
   styles: {
     src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
     dest: 'dist/assets/css'
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+    dest: 'dist/assets/images'
   }
 }
 
 export const styles = () => {
 
   let sass = require('gulp-sass')(require('sass'));
+ 
+ 
 
   return gulp.src(paths.styles.src)
     .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
@@ -28,6 +35,9 @@ export const styles = () => {
     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+
+
 
 export const watch = () => {
   gulp.watch( 'src/assets/scss/**/*.scss', styles);
