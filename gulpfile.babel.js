ea4b5bc4b7eecc223682d1a3b0ecae88bad9d8c0
Michael Kidby <kidbymichaelb@gmail.com>
2021-08-28 23:06:47 -0400

    gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,20 +3,34 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 
 
 
 const PRODUCTION = yargs.argv.prod;
 
+const paths = {
+  styles: {
+    src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+    dest: 'dist/assets/css'
+  }
+}
+
 export const styles = () => {
 
   let sass = require('gulp-sass')(require('sass'));
 
-  return gulp.src('src/assets/scss/bundle.scss')
+  return gulp.src(paths.styles.src)
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
-    .pipe(gulp.dest('dist/assets/css'));
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const watch = () => {
+  gulp.watch( 'src/assets/scss/**/*.scss', styles);
 }
 
 
--- lib/enqueue-styles.php
+++ lib/enqueue-styles.php
@@ -7,4 +7,11 @@ function mmdigital_assets() {
 
 add_action('wp_enqueue_scripts', 'mmdigital_assets');
 
+
+function mmdigital_admin_assets() {
+    wp_enqueue_style( 'mmdigital-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+}
+
+add_action('admin_enqueue_scripts', 'mmdigital_assets');
+
 ?>
