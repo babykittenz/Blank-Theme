4bfd56c29a87fc395de2ad043607989a526ba030
Michael Kidby <kidbymichaelb@gmail.com>
2021-09-19 22:33:53 -0400

    css arcitecture add

--- lib/enqueue-styles.php
+++ lib/enqueue-styles.php
@@ -5,7 +5,7 @@ function identitydental_assets() {
     wp_enqueue_style( 'identitydental-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
 }
 
-add_action('wp_enqueue_scripts', 'mmdigital_assets');
+add_action('wp_enqueue_scripts', 'identitydental_assets');
 
 
 function identitydental_admin_assets() {
