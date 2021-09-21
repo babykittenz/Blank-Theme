6f10f3f732a37853ebf05cd06cd18654e661f253
Michael Kidby <kidbymichaelb@gmail.com>
2021-09-21 00:19:17 -0400

    custom header text

--- header.php
+++ header.php
@@ -12,7 +12,16 @@
     <body <?php body_class(); ?>>
     <a  href=   "#content"  class=  "skip-link  screen-reader-text" title=""><?php esc_html_e(   'Skip to content'   ,   'identitydental'  ); ?></a>
         <header>
-            
+        <?php
+
+$header_text_one = get_theme_mod('identitydental_header_text_one', '');
+$header_text_two = get_theme_mod('identitydental_header_text_two', '');
+
+?>
+
+
+    <?php echo esc_html( $header_text_one ); ?>
+    <?php echo esc_html( $header_text_two ); ?>
             <div class="upper-bar">
                 <div><?php get_template_part( 'template-parts/header/nav' ); ?></div>
                 <div></div>
